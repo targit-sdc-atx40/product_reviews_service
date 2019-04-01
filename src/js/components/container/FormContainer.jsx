ad213b15a72b206f44160980032f3ccdf354d1db
David Castillo <d.v.castillo37@gmail.com>
2019-04-01 15:56:51 -0500

    Created the layout for the customer reviews and filled it fake sample data.

--- src/js/components/container/FormContainer.jsx
+++ src/js/components/container/FormContainer.jsx
@@ -1,34 +1,164 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-// import Input from "../presentational/Input.jsx";
+import StarRatings from "react-star-ratings";
 
 class FormContainer extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      // seo_title: ""
+     
     };
-    // this.handleChange = this.handleChange.bind(this);
+  
   }
-  // handleChange(event) {
-  //   this.setState({ [event.target.id]: event.target.value });
-  // }
+
   render() {
-    // const { seo_title } = this.state;
     return (
       <div>
-          <h1>Ratings &amp; Reviews</h1>
+        <div>
+          <center>
+            <h2>Ratings &amp; reviews <u>81</u></h2>
+            <StarRatings
+              rating={4}
+              starDimension="24px"
+              starSpacing="0px"
+              starRatedColor="gold"
+            /> 
+          </center>
+        </div>
+        <div>
+          <h3>Most helpful positive reviews</h3>
+          <div>
+            <table>
+              <tr><td>
+                <h4>Great Shirt</h4>
+                <div>(Would Recommend)</div>
+                <span>
+                <StarRatings
+                  rating={4}
+                  starDimension="12px"
+                  starSpacing="0px"
+                  starRatedColor="gold"
+                />
+                username - 1 year ago
+                </span>
+                <br></br>
+                <span>Love this shirt. The material has a nice feel to it, and isn't overly thin. My husband wears a medium shirt and prefers the slim fit style, but unfortunately the medium in this brand did not work for him. While it fit in the torso area, it was too snug in the shoulder and arm area. If you're purchasing this for someone that is a little more muscular, I would recommend having them try it on first in case you need to go up a size.</span>
+              </td>
+              <td>
+                <h4>Great Shirt</h4>
+                <div>(Would Recommend)</div>
+                <span>
+                <StarRatings
+                  rating={4}
+                  starDimension="12px"
+                  starSpacing="0px"
+                  starRatedColor="gold"
+                />
+                username - 1 year ago
+                </span>
+                <br></br>
+                <span>Love this shirt. The material has a nice feel to it, and isn't overly thin. My husband wears a medium shirt and prefers the slim fit style, but unfortunately the medium in this brand did not work for him. While it fit in the torso area, it was too snug in the shoulder and arm area. If you're purchasing this for someone that is a little more muscular, I would recommend having them try it on first in case you need to go up a size.</span>
+              </td></tr>
+              <tr><td>
+                <h4>Great Shirt</h4>
+                <div>(Would Recommend)</div>
+                <span>
+                <StarRatings
+                  rating={4}
+                  starDimension="12px"
+                  starSpacing="0px"
+                  starRatedColor="gold"
+                />
+                username - 1 year ago
+                </span>
+                <br></br>
+                <span>True to size and decent quality. Could be cheaper though.</span>
+              </td>
+              <td>
+                <h4>Great Shirt</h4>
+                <div>(Would Recommend)</div>
+                <span>
+                <StarRatings
+                  rating={4}
+                  starDimension="12px"
+                  starSpacing="0px"
+                  starRatedColor="gold"
+                />
+                username - 1 year ago
+                </span>
+                <br></br>
+                <span>Love this shirt. The material has a nice feel to it, and isn't overly thin. My husband wears a medium shirt and prefers the slim fit style, but unfortunately the medium in this brand did not work for him. While it fit in the torso area, it was too snug in the shoulder and arm area. If you're purchasing this for someone that is a little more muscular, I would recommend having them try it on first in case you need to go up a size.</span>
+              </td></tr>
+            </table>
+          </div>
+        </div>
+        <br></br>
+        <div>
+          <h3>Most recent reviews</h3>
+          <div>
+            <table>
+              <tr><td>
+                <h4>Great Shirt</h4>
+                <div>(Would Recommend)</div>
+                <span>
+                <StarRatings
+                  rating={4}
+                  starDimension="12px"
+                  starSpacing="0px"
+                  starRatedColor="gold"
+                />
+                username - 1 year ago
+                </span>
+                <br></br>
+                <span>Love this shirt. The material has a nice feel to it, and isn't overly thin. My husband wears a medium shirt and prefers the slim fit style, but unfortunately the medium in this brand did not work for him. While it fit in the torso area, it was too snug in the shoulder and arm area. If you're purchasing this for someone that is a little more muscular, I would recommend having them try it on first in case you need to go up a size.</span>
+              </td><td>
+                <h4>Great Shirt</h4>
+                <div>(Would Recommend)</div>
+                <span>
+                <StarRatings
+                  rating={4}
+                  starDimension="12px"
+                  starSpacing="0px"
+                  starRatedColor="gold"
+                />
+                username - 1 year ago
+                </span>
+                <br></br>
+                <span>Love this shirt. The material has a nice feel to it, and isn't overly thin. My husband wears a medium shirt and prefers the slim fit style, but unfortunately the medium in this brand did not work for him. While it fit in the torso area, it was too snug in the shoulder and arm area. If you're purchasing this for someone that is a little more muscular, I would recommend having them try it on first in case you need to go up a size.</span>
+              </td></tr>
+              <tr><td>
+                <h4>Great Shirt</h4>
+                <div>(Would Recommend)</div>
+                <span>
+                <StarRatings
+                  rating={4}
+                  starDimension="12px"
+                  starSpacing="0px"
+                  starRatedColor="gold"
+                />
+                username - 1 year ago
+                </span>
+                <br></br>
+                <span>Love this shirt. The material has a nice feel to it, and isn't overly thin. My husband wears a medium shirt and prefers the slim fit style, but unfortunately the medium in this brand did not work for him. While it fit in the torso area, it was too snug in the shoulder and arm area. If you're purchasing this for someone that is a little more muscular, I would recommend having them try it on first in case you need to go up a size.</span>
+              </td><td>
+                <h4>Great Shirt</h4>
+                <div>(Would Recommend)</div>
+                <span>
+                <StarRatings
+                  rating={4}
+                  starDimension="12px"
+                  starSpacing="0px"
+                  starRatedColor="gold"
+                />
+                username - 1 year ago
+                </span>
+                <br></br>
+                <span>Love this shirt. The material has a nice feel to it, and isn't overly thin. My husband wears a medium shirt and prefers the slim fit style, but unfortunately the medium in this brand did not work for him. While it fit in the torso area, it was too snug in the shoulder and arm area. If you're purchasing this for someone that is a little more muscular, I would recommend having them try it on first in case you need to go up a size.</span>
+              </td></tr>
+            </table>
+          </div>
+        </div>
       </div>
-      // <form id="article-form">
-      //   <Input
-      //     text="SEO title"
-      //     label="seo_title"
-      //     type="text"
-      //     id="seo_title"
-      //     value={seo_title}
-      //     handleChange={this.handleChange}
-      //   />
-      // </form>
     );
   }
 }
