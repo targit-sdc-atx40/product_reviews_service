ffa484acff61686cf46561c6de1e2c6505d51c6a
David Castillo <d.v.castillo37@gmail.com>
2019-04-08 20:04:13 -0500

    Page dynamically renders reviews based on sku

--- server/server.js
+++ server/server.js
@@ -1,12 +1,47 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const db = require('../database/db_config.js')
+const express = require('express');
+const app = express();
+const port = 3002;
+const db = require('../database/db_config.js');
+const {recentReviews} = require('../database/db_config.js');
+const {helpfulReviews} = require('../database/db_config.js');
+const {allReviews} = require('../database/db_config.js')
 app.use(express.json({urlencoded: true}));
-app.use(express.static('./dist'))
+app.use(express.static('./dist'));
 
-app.get('/product/reviews/recent', db.recent)
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-app.get('/product/reviews/helpful', db.helpful)
+app.get('/product/reviews/recent', (req, res) => {
+  console.log('in server', req.query.sku);
+  recentReviews(req.query.sku, (err, data) => {
+    if(err) {
+      console.error(error);
+    } else {
+      res.send(data.rows);
+    }
+  })
+});
+app.get('/product/reviews/helpful', (req, res) => {
+  helpfulReviews(req.query.sku, (err, data) => {
+    if(err) {
+      console.error(error);
+    } else {
+      res.send(data.rows);
+    }
+  })
+});
+app.get('/product/reviews/all', (req, res) => {
+  allReviews(req.query.sku, (err, data) => {
+    if(err) {
+      console.error(error);
+    } else {
+      res.send(data.rows);
+    }
+  })
+});
 
-app.listen(port, () => { console.log('Clean up on aisle ', port) })
+
+app.listen(port, () => { console.log('Clean up on aisle ', port) });
