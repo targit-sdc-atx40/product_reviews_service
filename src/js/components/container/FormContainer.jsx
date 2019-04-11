911c3242286e728d7f0cd0a4a2c61d85fb88b0ca
David Castillo <d.v.castillo37@gmail.com>
2019-04-11 13:26:59 -0500

    Ran into a styling error, placed the inline styling somewhere different to test

--- src/js/components/container/FormContainer.jsx
+++ src/js/components/container/FormContainer.jsx
@@ -105,10 +105,10 @@ class FormContainer extends Component {
   render() {
     window.reviews = this;
     return (
-      <div style={{fontFamily: Helvetica}}>
+      <div>
         <div>
           <center>
-            <h2>Ratings &amp; reviews <u>{this.state.totalReviews? this.state.totalReviews.length : 0}</u></h2>
+            <h2 style={{fontFamily: Helvetica}}>Ratings &amp; reviews <u>{this.state.totalReviews? this.state.totalReviews.length : 0}</u></h2>
             <StarRatings
               rating={this.state.totalRating}
               starDimension="24px"
