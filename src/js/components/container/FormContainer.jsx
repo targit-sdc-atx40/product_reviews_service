3c057fda80f4f77f60ddfa63c9646ee516ddc976
David Castillo <d.v.castillo37@gmail.com>
2019-04-12 16:28:12 -0500

    Test 2

--- src/js/components/container/FormContainer.jsx
+++ src/js/components/container/FormContainer.jsx
@@ -125,8 +125,7 @@ class FormContainer extends Component {
           <h3 style={{fontSize: '19px', fontWeight: 'bold', display: 'block'}}>Most helpful positive reviews</h3>
           <div>
             <table>
-              <tr><td style={{width: '677.5px'}}>
-                <div style={{marginLeft: '0'}}>
+              <tr><td style={{width: '677.5px', padding: '0'}}>
                 <h4 style={{fontSize: '16px', fontWeight: 'bold'}}>{this.state.helpful[0] ? this.state.helpful[0].header : null}</h4> 
                 <div style={{fontSize: '14px'}}>{this.state.helpful[0]? (this.state.helpful[0].stars >= 3 ? '(would recommend)' : '(would not recommend)') : null}</div> 
                 <span style={{fontSize: '12px'}}>
@@ -140,10 +139,8 @@ class FormContainer extends Component {
                 </span>
                 <br></br>
                 <div style={{paddingRight: '15px', fontSize: '14px'}}>{this.state.helpful[0] ? this.state.helpful[0].body : null}</div>
-                </div>
               </td>
               <td style={{width: '677.5px'}}>
-                <div style={{marginLeft: '0'}}>
                 <h4 style={{fontSize: '16px', fontWeight: 'bold', display: 'block'}}>{this.state.helpful[1] ? this.state.helpful[1].header : null}</h4> 
                 <div style={{fontSize: '14px'}}>{this.state.helpful[1]? (this.state.helpful[1].stars >= 3 ? '(would recommend)' : '(would not recommend)') : null}</div> 
                 <span style={{fontSize: '12px'}}>
@@ -157,11 +154,9 @@ class FormContainer extends Component {
                 </span>
                 <br></br>
                 <div style={{paddingRight: '15px', fontSize: '14px'}}>{this.state.helpful[1] ? this.state.helpful[1].body : null}</div>
-                </div>
               </td></tr>
               <br></br>
               <tr><td style={{width: '677.5px'}}>
-              <div style={{marginLeft: '0'}}>
                 <h4 style={{fontSize: '16px', fontWeight: 'bold'}}>{this.state.helpful[2] ? this.state.helpful[2].header : null}</h4> 
                 <div style={{fontSize: '14px'}}>{this.state.helpful[2]? (this.state.helpful[2].stars >= 3 ? '(would recommend)' : '(would not recommend)') : null}</div> 
                 <span style={{fontSize: '12px'}}>
@@ -175,10 +170,8 @@ class FormContainer extends Component {
                 </span>
                 <br></br>
                 <div style={{paddingRight: '15px', fontSize: '14px'}}>{this.state.helpful[2] ? this.state.helpful[2].body : null}</div>
-              </div>
               </td>
               <td style={{width: '677.5px'}}> 
-              <div style={{marginLeft: '0'}}>
                 <h4 style={{fontSize: '16px', fontWeight: 'bold'}}>{this.state.helpful[3] ? this.state.helpful[3].header : null}</h4> 
                 <div style={{fontSize: '14px'}}>{this.state.helpful[3]? (this.state.helpful[3].stars >= 3 ? '(would recommend)' : '(would not recommend)') : null}</div> 
                 <span style={{fontSize: '12px'}}>
@@ -192,7 +185,6 @@ class FormContainer extends Component {
                 </span>
                 <br></br>
                 <div style={{paddingRight: '15px', fontSize: '14px'}}>{this.state.helpful[3] ? this.state.helpful[3].body : null}</div>
-              </div>
               </td></tr>
             </table>
           </div>
